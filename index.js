3a988fe1756250fc7deace2f4633f1574eed9994
piyush539 <76058085+piyush539@users.noreply.github.com>
2022-08-07 18:59:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+   // console.log("hello world");
+  // console.warn("this is warning");
+   //console.error("this is error");
+
+   // js variable
+  // var number1 = 89;
+//    var number2 = 100;
+//    console.log(number1+number2)
+//    console.log(number1-number2)
+//    console.log(number1*number2)
+//    console.log(number1/number2)
+
+// data types in js
+// numbers
+
+//var num1 =50;
+// var num2 =30;
+
+// //string
+
+// var str1 ="i am shivay";
+// var str2="i am shiv";
+
+// //objects
+
+// var marks ={
+//     ravi:67,
+//     shub:55,
+//     saurabh:36.36
+// }
+// console.log(num1)
+// console.log(num2)
+// console.log(num1+num2)
+// console.log(str1)
+// console.log(str2)
+// console.log(marks)
+
+// dates in js
+
+// let mydate = new Date();
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getDay());
+// console.log(mydate.getMinutes());
+// console.log(mydate.getHours());
+
+// function clicked(){
+//     console.log('the button wa clicked')
+// }
+// let namestebtn = document.querySelector('button')
+// namestebtn.addEventListener('click',inputMsg);
+// function inputMsg(){
+//   let name = prompt('enter name of student');
+//   namestebtn.textContent='roll no. 1:'+name;
+// }
+
